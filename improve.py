bd8fb6dd0276d947425bcf3ae94999a23d69b7e9
Zo Obradovic <ping@obradovic.com>
2018-08-11 14:09:19 -0700

    Initial commit

---
+++ improve.py
@@ -0,0 +1,15 @@
+# The below function checks whether a string is a palindrome.
+# Please explain, in 250 words or less, how you'd improve the code and why.
+def is_palindrone(s):
+    r=""
+    for c in s:
+        r = c +r
+    for x in range(0, len(s)):
+        if s[x] == r[x]:
+            x = True
+        else:
+            return False
+    return x
+
+print(is_palindrone('abba'))
+print(is_palindrone('Abba'))
